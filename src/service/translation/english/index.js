d7adfe483b0c4dcf5d47180d73856b75fb57dbd6
vanmoortel <nolan@o2b.dev>
2021-12-30 20:01:52 +0100

    Enable Node.js Language Metrics

---
+++ src/service/translation/english/index.js
@@ -0,0 +1,8 @@
+export const MESSAGE = {
+    'Try again...\n\n': 'Try again...\n\n',
+    'Please select the emoji you see in the image': (successQty) =>`Please select the emoji you see in the image(${successQty}/3):`,
+    'Well done!\n': 'Well done!\n',
+    'You are now verified.': 'You are now verified.'
+}
+
+export default MESSAGE
